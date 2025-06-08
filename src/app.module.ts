5bd4283279c63a6b5421b88f74e4cf1167b17ff0
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-06-08 14:31:00 +0600

    course module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,12 @@ import { AuthModule } from './main/auth/auth.module';
 import { MailService } from './main/mail/mail.service';
 import { ContactModule } from './main/contact/contact.module';
 import { QuoteModule } from './main/quote/quote.module';
+import { CourseController } from './main/course/course.controller';
+import { CourseModule } from './main/course/course.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, ContactModule, QuoteModule],
-  controllers: [AppController],
+  imports: [PrismaModule, AuthModule, ContactModule, QuoteModule, CourseModule],
+  controllers: [AppController, CourseController],
   providers: [AppService, PrismaService, MailService],
 })
 export class AppModule {}

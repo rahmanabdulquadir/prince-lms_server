af48124138d00d461b496b7680cdb95cadaebb7b
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-06-10 11:08:43 +0600

    decorator added

--- src/main/user/user.controller.ts
+++ src/main/user/user.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Patch, Body, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
+@ApiTags('User')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('me')
+  getMe(@CurrentUser('id') userId: string) {
+    return this.userService.getMe(userId);
+  }
+
+  @Patch('me')
+  updateMe(@CurrentUser('id') userId: string, @Body() dto: UpdateUserDto) {
+    return this.userService.updateMe(userId, dto);
+  }
+}

9cc8b3e4fd222fcd74f0ae3dda21a9c374592be5
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-06-10 10:49:25 +0600

    user service added

--- src/main/user/user.service.ts
+++ src/main/user/user.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async getMe(userId: string) {
+    return this.prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        fullName: true,
+        email: true,
+        phoneNumber: true,
+        role: true,
+        isSubscribed: true,
+        createdAt: true,
+        updatedAt: true,
+        // add relations if needed
+      },
+    });
+  }
+
+  async updateMe(userId: string, dto: UpdateUserDto) {
+    return this.prisma.user.update({
+      where: { id: userId },
+      data: dto,
+    });
+  }
+}
+ 

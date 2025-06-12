33ce79c53043408b69121a95f32ff577fa8a3d55
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-06-12 08:51:51 +0600

    table created with new db url

--- src/main/user/user.controller.ts
+++ src/main/user/user.controller.ts
@@ -28,28 +28,28 @@ export class UserController {
   }
 
   @Patch('me')
-@UseInterceptors(FileInterceptor('file'))
-@ApiConsumes('multipart/form-data')
-@ApiBody({
-  schema: {
-    type: 'object',
-    properties: {
-      fullName: { type: 'string', example: 'John Doe' },
-      email: { type: 'string', format: 'email', example: 'john@example.com' },
-      phoneNumber: { type: 'string', example: '1234567890' },
-      file: {
-        type: 'string',
-        format: 'binary',
-        description: 'Profile photo (optional)',
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        fullName: { type: 'string', example: 'John Doe' },
+        email: { type: 'string', format: 'email', example: 'john@example.com' },
+        phoneNumber: { type: 'string', example: '1234567890' },
+        file: {
+          type: 'string',
+          format: 'binary',
+          description: 'Profile photo (optional)',
+        },
       },
     },
-  },
-})
-updateMe(
-  @CurrentUser('id') userId: string,
-  @UploadedFile() file: Express.Multer.File,
-  @Body() dto: UpdateUserDto,
-) {
-  return this.userService.updateMe(userId, dto, file);
-}
+  })
+  updateMe(
+    @CurrentUser('id') userId: string,
+    @UploadedFile() file: Express.Multer.File,
+    @Body() dto: UpdateUserDto,
+  ) {
+    return this.userService.updateMe(userId, dto, file);
+  }
 }

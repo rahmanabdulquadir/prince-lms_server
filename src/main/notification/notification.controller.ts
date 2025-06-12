81a8092ae62d61bd9affe80766f554b6040e3dd2
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-06-12 14:06:28 +0600

    controller and module added

--- src/main/notification/notification.controller.ts
+++ src/main/notification/notification.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Patch, UseGuards } from '@nestjs/common';
+import { NotificationService } from './notification.service';
+import { CreateNotificationDto } from './dto/create-notification.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
-@Controller('notification')
-export class NotificationController {}
+@ApiTags('Notification')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
+@Controller('notifications')
+export class NotificationController {
+  constructor(private readonly notificationService: NotificationService) {}
+
+  @Post()
+  create(@Body() dto: CreateNotificationDto) {
+    return this.notificationService.create(dto);
+  }
+
+  @Get()
+  findAllByUser(@CurrentUser('id') userId: string) {
+    return this.notificationService.findAllByUser(userId);
+  }
+
+  @Patch(':id/read')
+  markAsRead(@Param('id') id: string) {
+    return this.notificationService.markAsRead(id);
+  }
+}

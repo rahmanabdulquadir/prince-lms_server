b26f0558b038dfe37a870b251d64f5b6b9771735
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-08-07 11:11:47 +0600

    update controller issue resolved

--- src/main/update/update.controller.ts
+++ src/main/update/update.controller.ts
@@ -11,7 +11,8 @@ import { UpdateService } from './update.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { CreateUpcomingContentDto } from './dto/create-upcoming-content.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { AdminGuard } from 'src/common/guard/AdminGuard';
 
 @ApiTags('Updates')
 @Controller('updates')
@@ -19,7 +20,8 @@ export class UpdateController {
   constructor(private readonly updateService: UpdateService) {}
 
   @Post('upcoming-updates')
-  @UseGuards(JwtAuthGuard) // You can replace this with AdminGuard if needed
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, AdminGuard) // You can replace this with AdminGuard if needed
   @UseInterceptors(FileInterceptor('bannerImage'))
   @ApiConsumes('multipart/form-data')
   @ApiBody({
